d2bd0a31fa9d460a786b8211d075b2656f32e651
Diego González <diegoandresgonzalezdiaz@gmail.com>
2021-04-11 22:42:30 -0500

    sección de viajeros mail y footer terminadas

--- src/js/effect.js
+++ src/js/effect.js
@@ -1,10 +1,18 @@
-// Img rotate
-var imgRotate = document.getElementById('imgRotate')
-var sourceImgRotate = [
-    'src/img/recursos/chico.jpg',
-    'src/img/recursos/chico.jpg',
-    'src/img/recursos/chico.jpg'
-]
-
-
-    
+// Img rotate: SECTION TRAVELER
+var imgRotate = document.imgRotate.src = 'src/img/recursos/travel3.jpg'
+    var sourceImgRotate = [
+        'src/img/recursos/travel1.jpg',
+        'src/img/recursos/travel2.jpg',
+        'src/img/recursos/travel3.jpg'
+    ]
+    var indice = 0;
+    var speedRotateImg = 1000
+function imgSlider() {
+    document.imgRotate.src = sourceImgRotate[indice]
+    if(indice < sourceImgRotate.length - 1){
+        indice++;
+    }else{
+        indice = 0;
+    };
+}
+setInterval(imgSlider, speedRotateImg);
